6d8576a0ee97f52bb1b2b734f8e4ce06792b3c7c
Andrew Masih <armasih@outlook.com>
2021-05-23 21:58:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* two ways of creatig an Array! 1st using 'Literal Notation' [ ], the prefered option. 2nd and using 'Array Constructor'. Challenge: 1/ create an Array containging numbers 1 to 10, and assign it to a varible: myNumber. 2/ Use Literal Notation or Array Constructor. 3/ numbers 6 - 10 should be a string and 1 - 5 should numbers
+*/
+
+
+let myNumbers = [1, 2, 3, 4, 5, "6", "7", "8", "9", "10"];
+
+
+console.log (myNumbers);
